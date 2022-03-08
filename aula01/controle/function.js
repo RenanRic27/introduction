bbcd93d4d5e3bcf8e373a982948ec76c18e3225e
RenanRic27 <renanricardo2781@gmail.com>
2022-03-08 10:10:57 -0300

    commit

--- aula01/controle/function.js
+++ aula01/controle/function.js
@@ -9,14 +9,28 @@ $(document).ready(function(){
         //coletar as informaçoes digitadas / selecionadas no formulario 
         let dados = $('#form').serialize()
 
-        $.ajax({
+        $('#retorno').empty()
 
+        $.ajax({
+            
             type: 'POST' ,
             dataType: 'JSON',
             assync: true,
             data: dados,
             url: '../modelo/retorno.php',
-            success: function(){
+            success: function(dados){
+                
+                $('#retorno').append(` <div class="col-12 col-sm-8 col-md-6">
+                <div class="alert-primary">
+                    <h1 class="text-white text-center">
+                        ${dados.mensagem}
+                    </h1>
+                        <img src="../../img/${dados.tipo}" class="img-fluid">
+                </div>
+    
+                </div>
+                `)
+
 
             }
 
